12dfe8d27281a0a1e7e0357ab3212285d8723dd4
Sbusiso Mdingi <ss.mdingi@icloud.com>
2026-07-31 16:55:37 +0200

    test(web): remove demo authentication bootstrap

--- apps/web/src/setupTests.js
+++ apps/web/src/setupTests.js
@@ -3,16 +3,13 @@ import { configure } from '@testing-library/dom'
 
 configure({ asyncUtilTimeout: 5_000 })
 
-window.__CLAIMGUARD_AUTHENTICATION_MODE__ = "demo_headers"
 window.__CLAIMGUARD_ORGANISATION_URL_SCHEME__ = "https"
 window.__CLAIMGUARD_ORGANISATION_HOST__ = "claimguard.test"
 
-// simple global fetch mock can be overridden in tests
 if (!global.fetch) {
   global.fetch = () => Promise.resolve({ ok: true, json: async () => ({ available: false, report: null }) })
 }
 
-// ResizeObserver polyfill for ReactFlow (not available in jsdom)
 if (!global.ResizeObserver) {
   global.ResizeObserver = class ResizeObserver {
     observe() {}
