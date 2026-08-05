b653aac0bebfacd2af95dfa0cc475f70aaf874cd
Sbusiso Mdingi <ss.mdingi@icloud.com>
2026-08-05 13:28:24 +0200

    test(database): run repeated legacy access races in MySQL gate

--- packages/database/tests/mysql.integration.test.js
+++ packages/database/tests/mysql.integration.test.js
@@ -2,3 +2,4 @@ import "./mysql-core.integration.test.js";
 import "./case-workflow-mysql.integration.test.js";
 import "./case-workflow-rollback-mysql.integration.test.js";
 import "./legacy-first-access-matrix-mysql.integration.test.js";
+import "./legacy-first-access-concurrency-mysql.integration.test.js";
