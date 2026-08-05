f52a16e48626bddc3d72b11577a89ee23c754fea
Sbusiso Mdingi <ss.mdingi@icloud.com>
2026-08-05 12:22:10 +0200

    test(database): wire legacy first-access matrix into MySQL gate

--- packages/database/tests/mysql.integration.test.js
+++ packages/database/tests/mysql.integration.test.js
@@ -1,3 +1,4 @@
 import "./mysql-core.integration.test.js";
 import "./case-workflow-mysql.integration.test.js";
 import "./case-workflow-rollback-mysql.integration.test.js";
+import "./legacy-first-access-matrix-mysql.integration.test.js";
