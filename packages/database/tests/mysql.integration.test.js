e9f9b4c5c1624a3ba25cc63e25cb3f531fe93e0f
Sbusiso Mdingi <ss.mdingi@icloud.com>
2026-08-05 13:32:13 +0200

    test(database): run negative legacy linkage matrix

--- packages/database/tests/mysql.integration.test.js
+++ packages/database/tests/mysql.integration.test.js
@@ -4,3 +4,4 @@ import "./case-workflow-rollback-mysql.integration.test.js";
 import "./legacy-first-access-matrix-mysql.integration.test.js";
 import "./legacy-first-access-concurrency-mysql.integration.test.js";
 import "./legacy-first-access-rollback-mysql.integration.test.js";
+import "./legacy-first-access-negative-mysql.integration.test.js";
