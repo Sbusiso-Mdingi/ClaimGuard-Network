97af335ac03317927a05930279de5c120609e322
Sbusiso Mdingi <ss.mdingi@icloud.com>
2026-08-07 20:45:12 +0200

    test(assessment): include reassessment MySQL gate

--- packages/database/tests/mysql.integration.test.js
+++ packages/database/tests/mysql.integration.test.js
@@ -1,4 +1,5 @@
 import "./mysql-core.integration.test.js";
+import "./assessment-reassessment-mysql.integration.test.js";
 import "./case-workflow-mysql.integration.test.js";
 import "./case-workflow-rollback-mysql.integration.test.js";
 import "./legacy-first-access-matrix-mysql.integration.test.js";
